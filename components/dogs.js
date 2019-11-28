26e452d35860ec992cbee3536d42e06c972b2e9d
Kilpikalevi <43344268+t7tuta00@users.noreply.github.com>
2019-11-28 07:32:35 +0000

    Add files via upload

--- components/dogs.js
+++ components/dogs.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-//  Some fixed example data of dogs 
-let dogData = {
+//  Some fixed example data of dogs
+/*let dogData = {
     dogs: [{
         id: 1,
         name: "Max",
@@ -18,12 +18,22 @@ let dogData = {
         name: "Tucker",
         image: "https://media.giphy.com/media/bbshzgyFQDqPHXBo4c/giphy.gif"
     }]
+}*/
+
+//  Some fixed example data of dogs
+let FallData = {
+    dataF: [{
+        id: 1,
+        data: "".}]
 }
 
-//  Return all dog information 
-router.get('/', (req, res) => { res.json(dogData)});
+/*//  Return all dog information
+//router.get('/', (req, res) => { res.json(dogData)});*/
 
-//  Return information of a single dog 
+//Palauttaa kaiken datan
+router.get('/', (req, res) => { res.json(FallData)});
+
+/*//  Return information of a single dog
 router.get('/:dogId', (req, res) => {
     const resultDog = dogData.dogs.find(d => {
         if (d.id == req.params.dogId) {
@@ -41,15 +51,35 @@ router.get('/:dogId', (req, res) => {
     {
         res.json(resultDog);
     }
+})*/
+
+//Palauttaa informaation yhdestä datasta id:n perusteella
+router.get('/:id', (req, res) => {
+    const resultData = dogData.dataF.find(d => {
+        if (d.id == req.params.dataF) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    });
+    if(resultData === undefined)
+    {
+        res.sendStatus(404)
+    }
+    else
+    {
+        res.json(resultData);
+    }
 })
 
-/* Create a new dog 
+/*Create a new dog
     Expects the following data format
     {
-        name: string, 
+        name: string,
         image: string - whole url to image
     }
-*/
+
 router.post('/', (req, res) => {
 
     dogData.dogs.push({
@@ -57,7 +87,7 @@ router.post('/', (req, res) => {
         name: req.body.name,
         image: req.body.image
     })
-    
+
     res.sendStatus(201);
 });
 
@@ -67,3 +97,30 @@ router.delete('/:id', (req, res) => {
 })
 
 module.exports = router;
+*/
+
+//luo uusi data
+Create a new dataF
+    Expects the following data format
+    {
+        id: string,
+        data: string
+    }
+
+    router.post('/', (req, res) => {
+
+        FallData.dataF.push({
+            id: FallData.dataF.length + 1,
+            name: req.body.name,
+            image: req.body.image
+        })
+
+        res.sendStatus(201);
+    });
+
+    router.delete('/:id', (req, res) => {
+        FallData.dataF = FallData.dataF.filter(dataF => dataF.id != req.params.id);
+        res.sendStatus(200);
+    })
+
+    module.exports = router;
